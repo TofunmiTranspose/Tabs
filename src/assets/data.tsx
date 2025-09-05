547b4259b18ff642731f65d4fb705f54b2a98988
TofunmiTranspose <tofexzyno@gmail.com>
2025-09-05 03:18:58 +0100

    Update data.tsx

--- src/assets/data.tsx
+++ src/assets/data.tsx
@@ -13,22 +13,22 @@ export const data: {
     icon: <FaHome />,
     label: "Home",
     content: (
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6 p-2 md:px-4 mx-aut0 ">
-        {Array.from({ length: 6 }).map((_, i) => (
-          <div className="max-w-sm m-2 mx-aut shadow" key={i}>
-            <img src='https://placehold.co/200' className="object-cover w-full h-30" alt="" />
-            <div className="div p-2">
-              <div className="mb-2">
-                <h2 className="font-bold text-sm sm:text-base">Amazing Card</h2>
-                <p className="text-gray-700 text-[0.6rem] sm:text-xs">
-                  This is a description for the amazing card.
-                </p>
-              </div>
-              <button className="px-2 sm:px-4 py-1 sm:py-2 bg-black text-white rounded text-[0.6rem] sm:text-xs cursor-pointer">Learn More</button>
-            </div>
-          </div>
-        ))}
+      <<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6 p-2 md:px-4 mx-aut0 dark:bg-gray-900">
+  {Array.from({ length: 6 }).map((_, i) => (
+    <div className="max-w-sm m-2 mx-aut shadow bg-white dark:bg-gray-800 dark:shadow-lg dark:shadow-gray-700/50" key={i}>
+      <img src='https://placehold.co/200' className="object-cover w-full h-30" alt="" />
+      <div className="div p-2">
+        <div className="mb-2">
+          <h2 className="font-bold text-sm sm:text-base text-black dark:text-gray-100">Amazing Card</h2>
+          <p className="text-gray-700 text-[0.6rem] sm:text-xs dark:text-gray-400">
+            This is a description for the amazing card.
+          </p>
+        </div>
+        <button className="px-2 sm:px-4 py-1 sm:py-2 bg-black text-white rounded text-[0.6rem] sm:text-xs cursor-pointer dark:bg-indigo-600 dark:text-white dark:hover:bg-indigo-700">Learn More</button>
       </div>
+    </div>
+  ))}
+</div>
     ),
   },
   {
